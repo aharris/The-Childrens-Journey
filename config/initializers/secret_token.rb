68362bccedb1956fafc9c2f4ca1dfb38f5d5edf6
Ashton Harris <ahdesignsonline@gmail.com>
2012-03-10 11:15:59 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheChildrensJourney::Application.config.secret_token = '8ad04d1c2784824ef30a18c9e3722d939595d06f3b8d727763839341d32080d16682c94daed84ad35e68cc5106a73b95850fed04bc7d2cf555ed4819e0b87f62'
